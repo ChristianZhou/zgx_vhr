617bd0aeae95b88812cfd1d8d78a556ab06dbfa1
CristianChou <18261937866m@sina.cn>
2020-10-09 22:53:41 +0800

    router调整

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from "@/views/Login";
+import Home from "@/views/Home";
 
 Vue.use(VueRouter)
 
 const routes = [
+    {
+        path: '/',
+        name: 'Login',
+        component: Login,
+        hidden: true
+    }, {
+        path: '/home',
+        name: 'Home',
+        component: Home,
+        hidden: true,
+        meta: {
+            roles: ['admin', 'user']
+        }
+    }
 ]
 
 const router = new VueRouter({
