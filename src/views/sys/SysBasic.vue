1836bf24297ac5b84a38419de3e0cd4596817e3a
CristianChou <18261937866m@sina.cn>
2020-10-11 00:10:49 +0800

    职位管理

--- src/views/sys/SysBasic.vue
+++ src/views/sys/SysBasic.vue
@@ -1,19 +1,19 @@
 <template>
 	<div>
 		<el-tabs type="border-card" v-model="activeName">
-			<el-tab-pane label="部门管理" name="first">
+			<el-tab-pane label="部门管理" name="depMana">
 				<DepMana></DepMana>
 			</el-tab-pane>
-			<el-tab-pane label="职位管理" name="second">
+			<el-tab-pane label="职位管理" name="poMana">
 				<PoMana></PoMana>
 			</el-tab-pane>
-			<el-tab-pane label="职称管理">
+			<el-tab-pane label="职称管理" name="jobLevelMana">
 				<JobLevelMana></JobLevelMana>
 			</el-tab-pane>
-			<el-tab-pane label="奖惩规则">
+			<el-tab-pane label="奖惩规则" name="ecMana">
 				<EcMana></EcMana>
 			</el-tab-pane>
-			<el-tab-pane label="权限组">
+			<el-tab-pane label="权限组" name="permissionMana">
 				<PermissionMana></PermissionMana>
 			</el-tab-pane>
 		</el-tabs>
@@ -32,7 +32,7 @@ export default {
 	name: "SysBasic",
 	data() {
 		return {
-			activeName: 'second'
+			activeName: 'jobLevelMana'
 		}
 	},
 	components: {
